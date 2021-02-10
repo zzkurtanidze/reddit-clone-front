d365a08970e4da7e9bbccb4e993a7dac21c67d7e
Zura Kurtanidze <zura.kurta@gmail.com>
2021-02-10 22:05:41 +0400

    #1 - 🎨 Add Post form page.

---
+++ src/pages/submit-page/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Container from "../../components/common/Container";
+import NewPostForm from "../../components/post-form/NewPostForm";
+
+export default function SubmitPage() {
+  return (
+    <Container>
+      <NewPostForm />
+    </Container>
+  );
+}
