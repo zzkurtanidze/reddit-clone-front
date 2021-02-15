64fdf02939c1244a32290914200938e4bdccdc21
zura12337 <zura.kurta@gmail.com>
2021-02-15 22:44:44 +0400

    🔥 Remove old code.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,48 +9,8 @@ import Loading from "../../components/common/Loading";
 import NewPostTeaser from "../../components/post-form/NewPostTeaser";
 import Container from "../../components/common/Container";
 
-const items = [
-  {
-    title: "Tom Bray",
-    body:
-      "here is description of this post and this description should be long enough I think this is enough.",
-    image:
-      "https://images.unsplash.com/photo-1610944770887-c470bab58def?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8dG93SlpGc2twR2d8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60j",
-    postedTo: "pewdiewpiecrew",
-    postedBy: "Jorj Janson",
-    votes: 150,
-  },
-  {
-    title: "Tom Bray",
-    body: "some dummy txt",
-    image:
-      "https://images.unsplash.com/photo-1610944770887-c470bab58def?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8dG93SlpGc2twR2d8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    postedTo: "pewdiewpiecrew",
-    postedBy: "Jorj Janson",
-    votes: 150,
-  },
-  {
-    title: "Tom Bray",
-    body: "some dummy txt",
-    image:
-      "https://images.unsplash.com/photo-1610902422826-548d3472fff5?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDl8dG93SlpGc2twR2d8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    postedTo: "pewdiewpiecrew",
-    postedBy: "Jorj Janson",
-    votes: 150,
-  },
-  {
-    title: "Tom Bray",
-    body: "some dummy txt",
-    image: "https://i.ytimg.com/vi/4m8LLfLXvMM/maxresdefault.jpg",
-    postedTo: "pewdiewpiecrew",
-    postedBy: "Jorj Janson",
-    votes: 150,
-  },
-];
-
 export default function HomePage() {
   const [posts, setPosts] = useState<PostType[]>([]);
-  const [error, setError] = useState();
   const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
@@ -60,11 +20,10 @@ export default function HomePage() {
   const fetchPosts = async () => {
     setLoading(true);
     const response = await getPosts();
-    console.log(response);
     if (response && response.statusText === "OK") {
       setPosts(response.data);
     } else {
-      setError(response?.data);
+      console.log(response?.data);
     }
     setLoading(false);
   };
@@ -77,6 +36,7 @@ export default function HomePage() {
           <Trending items={posts.length >= 6 ? posts.slice(0, 4) : posts} />
         )}
         <Grid mt={10} templateColumns="1fr 0.5fr" gap={50}>
+          {}
           <Box>
             <NewPostTeaser />
             {posts.length >= 1 &&
