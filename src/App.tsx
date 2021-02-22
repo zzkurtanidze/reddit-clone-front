062d8123377e0d15b750a7cb28c74d6fc8ace52f
zura12337 <zura.kurta@gmail.com>
2021-02-22 20:20:41 +0400

    #6 - 🛡  Add protected route for drafts.

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import { Fonts } from "./Fonts";
 import ProtectedRoute from "./components/common/ProtectedRoute";
 import PostPage from "./pages/post-page";
 import DraftsPage from "./pages/submit-page/drafts";
+import PostDraftsPage from "./pages/submit-page/drafts";
 
 const theme = extendTheme({
   fonts: {
@@ -43,7 +44,7 @@ export default function App() {
       <UserContext.Provider value={user}>
         <NavBar />
         <Switch>
-          <ProtectedRoute path="/submit/drafts" component={DraftsPage} />
+          <ProtectedRoute path="/submit/drafts" component={PostDraftsPage} />
           <ProtectedRoute path="/submit" component={SubmitPage} />
           <Route path="/user/:id" component={UserPage} />
           <Route path="/post/:id" component={PostPage} />
