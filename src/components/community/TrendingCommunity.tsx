f1240162dc5c625f71486ae7aec34a0239e41408
Zura <zura.kurta@gmail.com>
2021-03-15 20:25:10 +0400

    :lipstick: Fix trending community whitespaces.

--- src/components/community/TrendingCommunity.tsx
+++ src/components/community/TrendingCommunity.tsx
@@ -28,28 +28,30 @@ export const TrendingCommunity: React.FC<{ community: CommunityType }> = ({
 
   return (
     <Flex justifyContent="space-between" gridGap={2}>
-      {community && (
-        <CommunityPicture
-          communityUsername={community.username}
-          imageSrc={community.image}
-          width="35px"
-          withLink
-        />
-      )}
-      <Box>
-        <Link
-          _focus={{}}
-          href={`/r/${community.name.split(" ").join("")}`}
-          letterSpacing={-0.2}
-          textOverflow="ellipsis"
-          noOfLines={1}
-          fontWeight="bold"
-          fontSize={12}
-        >
-          r/{community.name}
-        </Link>
-        <Text fontSize={10}>{joinedNumber} Members</Text>
-      </Box>
+      <Flex gridGap={2}>
+        {community && (
+          <CommunityPicture
+            communityUsername={community.username}
+            imageSrc={community.image}
+            width="35px"
+            withLink
+          />
+        )}
+        <Box>
+          <Link
+            _focus={{}}
+            href={`/r/${community.name.split(" ").join("")}`}
+            letterSpacing={-0.2}
+            textOverflow="ellipsis"
+            noOfLines={1}
+            fontWeight="bold"
+            fontSize={12}
+          >
+            r/{community.name}
+          </Link>
+          <Text fontSize={10}>{joinedNumber} Members</Text>
+        </Box>
+      </Flex>
       <Join community={community} />
       <LoginModal setShowModal={setLoginModal} showModal={loginModal} />
     </Flex>
