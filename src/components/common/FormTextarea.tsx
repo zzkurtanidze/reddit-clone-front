6443ebc083783af88a5f1825913abd15276fa195
Zura <zura.kurta@gmail.com>
2021-03-17 21:17:52 +0400

    :sparkles: Create FormTextarea component.

---
+++ src/components/common/FormTextarea.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Textarea,
+} from "@chakra-ui/react";
+
+export default function FormTextarea({
+  placeholder,
+  label,
+  name,
+  error,
+  required = false,
+  description,
+}: {
+  placeholder?: string;
+  label?: string;
+  name: string;
+  error: string | undefined;
+  required?: boolean;
+  description?: string;
+}) {
+  return (
+    <FormControl isInvalid={error ? true : false} my={2}>
+      {label && (
+        <FormLabel fontFamily="mono">{`${label} ${
+          required ? "*" : ""
+        }`}</FormLabel>
+      )}
+      {description && (
+        <FormHelperText fontSize={12} fontFamily="mono" mb={2}>
+          {description}
+        </FormHelperText>
+      )}
+      <Textarea
+        placeholder={placeholder && `${placeholder} ${required ? "*" : ""}`}
+        name={name}
+      ></Textarea>
+    </FormControl>
+  );
+}
