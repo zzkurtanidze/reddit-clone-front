22957ad923bc0bceceb56a72a7bc78c546797055
Zura <zura.kurta@gmail.com>
2021-03-21 23:39:07 +0400

    :sparkles: Add reset password functional.

--- src/components/form-modals/ForgotPasswordModal.tsx
+++ src/components/form-modals/ForgotPasswordModal.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@chakra-ui/button";
 import { Flex, Text } from "@chakra-ui/layout";
+import { useToast } from "@chakra-ui/toast";
 import { Form, Formik } from "formik";
-import { expectsResolvedDragConstraints } from "framer-motion/types/gestures/drag/VisualElementDragControls";
-import { valueScaleCorrection } from "framer-motion/types/render/dom/projection/scale-correction";
 import React from "react";
+import { resetPassword } from "../../api";
 import FormField from "../common/FormField";
-import PrimaryButton from "../common/PrimaryButton";
 import Modal from "../Modal";
 
 export default function ForgotPasswordModal({
@@ -15,21 +14,53 @@ export default function ForgotPasswordModal({
   showModal: boolean;
   setShowModal: Function;
 }) {
+  const toast = useToast();
+
+  const handleReset = async (data: { username: string; email: string }) => {
+    const response = await resetPassword(data);
+
+    if (response.statusText === "OK") {
+      toast({
+        title: "Email has been sent succesfully",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: response.data,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
   return (
     <Modal open={showModal} width="500px" onClose={() => setShowModal(false)}>
-      <Text fontWeight="bold">Forgot your password?</Text>
+      <img
+        src="https://www.redditstatic.com/accountmanager/18e257d5fdea817c0f12cccf8867d930.svg"
+        alt="Reddit-logo"
+      />
       <br />
+      <Text fontWeight="bold">Reset your password</Text>
       <Text fontSize={14} fontFamily="mono">
-        Enter your email address
+        Tell us the username and email address associated with your Reddit
+        account, and we’ll send you an email with a link to reset your password.
       </Text>
+      <br />
       <Formik
-        initialValues={{ email: "" }}
-        onSubmit={(data) => console.log(data)}
-        validate={({ email }) => {
+        initialValues={{ username: "", email: "" }}
+        onSubmit={async (data) => handleReset(data)}
+        validate={({ username, email }) => {
           const errors: Record<string, string> = {};
 
           if (email.length < 1) {
             errors.email = "Enter email.";
+          }
+
+          if (username.length < 1) {
+            errors.username = "Enter username.";
           }
 
           return errors;
@@ -39,7 +70,13 @@ export default function ForgotPasswordModal({
       >
         {({ errors }) => (
           <Form>
-            <Flex direction="column" gridGap={2}>
+            <Flex direction="column" gridGap={3}>
+              <FormField
+                placeholder="Username"
+                name="username"
+                type="input"
+                error={errors.username}
+              />
               <FormField
                 placeholder="Email"
                 name="email"
