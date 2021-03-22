12cfd388b0af0d27158042b622bf35718553522d
Zura <zura.kurta@gmail.com>
2021-03-23 02:58:14 +0400

    :zap: #18 - Validate url.

--- src/components/posts/post-form/tabs/LinkTab.tsx
+++ src/components/posts/post-form/tabs/LinkTab.tsx
@@ -1,11 +1,39 @@
+import { FormControl, FormErrorMessage } from "@chakra-ui/form-control";
 import { Box } from "@chakra-ui/layout";
 import { Textarea } from "@chakra-ui/textarea";
-import React from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 
-export default function LinkTab() {
+export default function LinkTab({
+  value,
+  onChange,
+}: {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+}) {
+  const [error, setError] = useState<string | undefined>();
+
+  useEffect(() => {
+    const urlReg = /https?:\/\/w{0,3}\w*?\.(\w*?\.)?\w{2,3}\S*|www\.(\w*?\.)?\w*?\.\w{2,3}\S*|(\w*?\.)?\w*?\.\w{2,3}[\/\?]\S*/;
+    if (value !== "") {
+      if (!urlReg.test(value)) {
+        setError("You did not entered valid URL.");
+      } else {
+        setError(undefined);
+      }
+    }
+  }, [value]);
+
   return (
     <Box my={2}>
-      <Textarea placeholder="Url" name="link" />
+      <FormControl isInvalid={error ? true : false}>
+        <Textarea
+          placeholder="Url"
+          name="link"
+          value={value}
+          onChange={onChange}
+        />
+        {error && <FormErrorMessage>{error}</FormErrorMessage>}
+      </FormControl>
     </Box>
   );
 }
