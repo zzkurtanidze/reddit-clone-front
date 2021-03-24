06dbc43b21dbfa14227bc45b62fbd91a2a217ba5
Zura <zura.kurta@gmail.com>
2021-03-25 01:25:02 +0400

    :sparkles: #25 - Add Rules Component to post page.

--- src/pages/submit-page/index.tsx
+++ src/pages/submit-page/index.tsx
@@ -1,12 +1,16 @@
+import { Grid } from "@chakra-ui/layout";
+import RulesSidebar from "../../components/Rules";
 import React from "react";
 import Container from "../../components/common/Container";
 import NewPostForm from "../../components/posts/post-form/NewPostForm";
 
 export default function SubmitPage({ match }: { match?: any }) {
-  console.log(match);
   return (
     <Container>
-      <NewPostForm match={match} />
+      <Grid gridTemplateColumns="1fr 0.4fr" gridGap={5}>
+        <NewPostForm match={match} />
+        <RulesSidebar />
+      </Grid>
     </Container>
   );
 }
