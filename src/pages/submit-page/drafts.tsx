b98d85e857a80ba4ed83da3ef35d793498fd5f81
Zura <zura.kurta@gmail.com>
2021-03-24 16:52:47 +0400

    :sparkles: #17 - Remove draft.

--- src/pages/submit-page/drafts.tsx
+++ src/pages/submit-page/drafts.tsx
@@ -1,25 +1,38 @@
 //@ts-nocheck
+import { useToast } from "@chakra-ui/toast";
 import React, { useEffect, useState } from "react";
-import { getDraftPosts } from "../../api";
+import { getDraftPosts, removeDraftPost } from "../../api";
 import Container from "../../components/common/Container";
 import DraftsList from "../../components/posts/post-form/drafts/DraftsList";
 import { NotPostedPostType } from "../../types";
 
 export default function PostDraftsPage() {
   const [drafts, setDrafts] = useState<NotPostedPostType>();
-  const handleRemove = (draft) => {
-    let posts = JSON.parse(localStorage.getItem("postDrafts")) || [];
 
-    let index = posts.findIndex((post) => post.title === draft.title);
-    posts.splice(index, 1);
-
-    // localStorage.setItem("postDrafts", JSON.stringify(posts));
-    setDrafts(posts);
-  };
+  const toast = useToast();
 
   useEffect(() => {
     fetchDrafts();
   }, []);
+
+  const handleRemove = async (draft) => {
+    const response = await removeDraftPost(draft._id);
+    console.log(response);
+    if (response.statusText === "OK") {
+      toast({
+        title: response.data,
+        status: "success",
+      });
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    } else {
+      toast({
+        title: response.data,
+        status: "error",
+      });
+    }
+  };
 
   const fetchDrafts = async () => {
     const response = await getDraftPosts();
