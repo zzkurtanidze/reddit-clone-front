86f72ac08b598dd97d0bc1dd916b82cb203ffe94
Zura <zura.kurta@gmail.com>
2021-03-28 03:20:19 +0400

    :sparkles: #20 - Add Tabs to settings page.

--- src/pages/user-page/user-settings.tsx
+++ src/pages/user-page/user-settings.tsx
@@ -1,12 +1,19 @@
+//@ts-nocheck
 import { Box, Flex, Text } from "@chakra-ui/layout";
 import Container from "../../components/common/Container";
 import React, { useEffect, useState } from "react";
 
 import { IoIosSettings } from "react-icons/io";
 import { Link } from "react-router-dom";
+import AccountTab from "./tabs/AccountTab";
+import ProfileTab from "./tabs/ProfileTab";
 
 export default function UserSettingsPage({ match }: { match: any }) {
   const [tab, setTab] = useState<string>("");
+  const tabs = {
+    account: <AccountTab />,
+    profile: <ProfileTab />,
+  };
 
   useEffect(() => {
     setTab(match.params.tab);
@@ -38,6 +45,9 @@ export default function UserSettingsPage({ match }: { match: any }) {
         <TabLink name="notifications" label="Notifications" tab={tab} />
         <TabLink name="premium" label="Subscriptions" tab={tab} />
       </Flex>
+      <Box my={10} w="60%">
+        {tabs[tab]}
+      </Box>
     </Container>
   );
 }
@@ -52,7 +62,13 @@ const TabLink = ({
   tab: string;
 }) => {
   return (
-    <Box px={5} py={3} borderBottom={tab === name ? "3px solid #0079D3" : "0"}>
+    <Box
+      px={5}
+      py={2}
+      color={tab === name ? "black" : "gray.500"}
+      _hover={{ color: "black" }}
+      borderBottom={tab === name ? "3px solid #0079D3" : "0"}
+    >
       <Link to={`/settings/${name}`}>{label}</Link>
     </Box>
   );
