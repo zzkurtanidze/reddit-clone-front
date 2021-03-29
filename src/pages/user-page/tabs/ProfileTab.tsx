2674281fdc3162ee2b7060828444fb07ec952a49
Zura <zura.kurta@gmail.com>
2021-03-30 00:48:08 +0400

    :boom: #20 - Upload profile and cover images from settings page.

--- src/pages/user-page/tabs/ProfileTab.tsx
+++ src/pages/user-page/tabs/ProfileTab.tsx
@@ -8,18 +8,25 @@ import SectionTitle from "../../../components/common/SectionTitle";
 import Title from "../../../components/common/Title";
 import { Image } from "@chakra-ui/image";
 import { UserType } from "../../../types/";
-import SecondaryButton from "../../../components/common/SecondaryButton";
 import { FaEdit } from "react-icons/fa";
 import _ from "lodash";
 import { updateUser } from "../../../api/";
 import { useToast } from "@chakra-ui/toast";
 import { Input } from "@chakra-ui/input";
 import { FormLabel } from "@chakra-ui/form-control";
-import ChangePicture from "../../../components/user/user-modals/ChangePicture";
-import UserPictureButton from "../../../components/user/UserPictureButton";
+import { Button } from "@chakra-ui/button";
+import ChangeUserPicture from "../../../components/user/common/ChangeUserPicture";
+import ChangeUserCover from "../../../components/user/common/ChangeUserCover";
 
 export default function ProfileTab({ user }: { user: UserType }) {
   const [changeCover, setChangeCover] = useState(false);
+  const [showProfileChangeModal, setShowProfileChangeModal] = useState<boolean>(
+    false
+  );
+  const [showCoverChangeModal, setShowCoverChangeModal] = useState<boolean>(
+    false
+  );
+
   const toast = useToast();
 
   return (
@@ -77,46 +84,57 @@ export default function ProfileTab({ user }: { user: UserType }) {
         label="Avatar and banner image"
         description="Images must be .png or .jpg format"
       />
-      <Formik
-        initialValues={{ profile: "", cover: "" }}
-        onSubmit={(data) => console.log(data)}
-      >
-        {({ errors }) => (
-          <Form>
-            <Flex gridGap={3}>
-              <Box position="relative">
-                <UserPictureButton image={user.image} width="125px" />
-              </Box>
-              <Box position="relative">
-                <Image
-                  src={
-                    user.coverImage ||
-                    "https://www.zipjob.com/blog/wp-content/uploads/2020/08/linkedin-default-background-cover-photo-1.png"
-                  }
-                  alt={user.username}
-                  h={125}
-                  borderRadius="10px"
-                />
-                <Input type="file" display="none" id="profile-pic" />
-                <FormLabel htmlFor="profile-pic">
-                  <Box
-                    cursor="pointer"
-                    position="absolute"
-                    bottom="15px"
-                    right="5px"
-                    p="10px"
-                    bg="white"
-                    border="1px solid #0079D3"
-                    borderRadius={50}
-                  >
-                    <FaEdit color="#0079D3" />
-                  </Box>
-                </FormLabel>
-              </Box>
-            </Flex>
-          </Form>
-        )}
-      </Formik>
+      <Flex gridGap={3}>
+        <Box position="relative">
+          <Image src={user.image} w={125} h={125} borderRadius="10px" />
+          <Button
+            position="absolute"
+            bottom="5px"
+            right="5px"
+            p="10px"
+            bg="white"
+            border="1px solid #0079D3"
+            onClick={() => setShowProfileChangeModal(true)}
+            borderRadius={50}
+          >
+            <FaEdit color="#0079D3" />
+          </Button>
+          <ChangeUserPicture
+            showProfileChangeModal={showProfileChangeModal}
+            setShowProfileChangeModal={setShowProfileChangeModal}
+          />
+        </Box>
+        <Box position="relative">
+          <Image
+            src={
+              user.coverImage ||
+              "https://www.zipjob.com/blog/wp-content/uploads/2020/08/linkedin-default-background-cover-photo-1.png"
+            }
+            alt={user.username}
+            w={500}
+            objectFit="cover"
+            h={125}
+            borderRadius="10px"
+          />
+          <Button
+            cursor="pointer"
+            position="absolute"
+            bottom="5px"
+            right="5px"
+            p="10px"
+            bg="white"
+            border="1px solid #0079D3"
+            borderRadius={50}
+            onClick={() => setShowCoverChangeModal(true)}
+          >
+            <FaEdit color="#0079D3" />
+          </Button>
+          <ChangeUserCover
+            showCoverChangeModal={showCoverChangeModal}
+            setShowCoverChangeModal={setShowCoverChangeModal}
+          />
+        </Box>
+      </Flex>
     </Box>
   );
 }
