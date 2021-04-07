396f305d1950c4d5c4e1ac8cbce0cf798b0693b4
Zura <zura.kurta@gmail.com>
2021-04-08 02:52:14 +0400

    :sparkles: #50 - Create categories component.

---
+++ src/components/Categories.tsx
@@ -0,0 +1,108 @@
+//@ts-ignore
+import { getCategories } from "@api/";
+import { Button } from "@chakra-ui/button";
+import { Box, Divider, Text } from "@chakra-ui/layout";
+//@ts-ignore
+import { CategoryType } from "@types/";
+import React, { useState } from "react";
+import StyledBox from "./common/StyledBox";
+import { CategoryButton } from "./posts/post-form/common/CategoryButton";
+
+export default function Categories() {
+  const [selected, setSelected] = useState<string>("");
+  const [expanded, setExpanded] = useState<boolean>(false);
+  const { categories, isLoading } = getCategories();
+
+  return (
+    <StyledBox maxW="15vw" fontWeight="medium" p={0} fontFamily="mono">
+      <Box bg="gray.100" py={2} px={4}>
+        <Text fontSize={18}>Categories</Text>
+      </Box>
+      <Divider />
+      <Button
+        textAlign="left"
+        fontSize={14}
+        fontWeight="bold"
+        justifyContent="flex-start"
+        borderRadius={0}
+        _focus={{}}
+        _active={{}}
+        w="100%"
+        boxShadow={selected === "All" ? "inset 5px 0px 0px #0079D3" : "0"}
+        bg={selected === "All" ? "gray.100" : "transparent"}
+        onClick={() => setSelected("All")}
+      >
+        All Categories
+      </Button>
+      {categories &&
+        categories.map((category: CategoryType, i: number) => (
+          <React.Fragment key={category.name}>
+            {!expanded ? (
+              i <= 7 && (
+                <>
+                  <Divider />
+                  <Button
+                    textAlign="left"
+                    fontSize={14}
+                    fontWeight="bold"
+                    justifyContent="flex-start"
+                    borderRadius={0}
+                    _focus={{}}
+                    _active={{}}
+                    w="100%"
+                    boxShadow={
+                      selected === category.name
+                        ? "inset 5px 0px 0px #4C6CD8 "
+                        : ""
+                    }
+                    bg={selected === category.name ? "gray.100" : "transparent"}
+                    onClick={() => setSelected(category.name)}
+                  >
+                    {category.name}
+                  </Button>
+                </>
+              )
+            ) : (
+              <>
+                <Divider />
+                <Button
+                  textAlign="left"
+                  fontSize={14}
+                  fontWeight="bold"
+                  justifyContent="flex-start"
+                  borderRadius={0}
+                  _focus={{}}
+                  _active={{}}
+                  w="100%"
+                  boxShadow={
+                    selected === category.name
+                      ? "inset 5px 0px 0px #4C6CD8 "
+                      : ""
+                  }
+                  bg={selected === category.name ? "gray.100" : "transparent"}
+                  onClick={() => setSelected(category.name)}
+                >
+                  {category.name}
+                </Button>
+              </>
+            )}
+          </React.Fragment>
+        ))}
+      <Button
+        w="100%"
+        borderRadius={50}
+        height={0}
+        py="15px"
+        fontSize={15}
+        color="#4C6CD8"
+        bg="none"
+        _focus={{}}
+        _active={{}}
+        transition="0s"
+        onClick={() => setExpanded(!expanded)}
+      >
+        {expanded ? "Show Less" : "Show More"}
+      </Button>
+    </StyledBox>
+  );
+}
--- src/pages/subreddits/index.tsx
+++ src/pages/subreddits/index.tsx
@@ -1,11 +1,12 @@
 import { Text } from "@chakra-ui/layout";
+import Categories from "@components/Categories";
 import Container from "@components/common/Container";
 import React from "react";
 
 export default function SubredditsPage() {
   return (
     <Container>
-      <Text>This is subreddits page</Text>
+      <Categories />
     </Container>
   );
 }
