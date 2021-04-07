39a6c3fc8efd5381e48c6da92b67fc2cea01961e
Zura <zura.kurta@gmail.com>
2021-04-08 00:34:10 +0400

    :sparkles: #54 - Create blank subreddits page.

---
+++ src/pages/subreddits/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@chakra-ui/layout";
+import Container from "@components/common/Container";
+import React from "react";
+
+export default function SubredditsPage() {
+  return (
+    <Container>
+      <Text>This is subreddits page</Text>
+    </Container>
+  );
+}
