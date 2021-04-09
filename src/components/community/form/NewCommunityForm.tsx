daccc3f811b26a39128c0b27f87b8a651c7106aa
Zura <zura.kurta@gmail.com>
2021-04-09 21:44:15 +0400

    :sparkles: #51 - Add Category field to community form.

--- src/components/community/form/NewCommunityForm.tsx
+++ src/components/community/form/NewCommunityForm.tsx
@@ -1,14 +1,29 @@
-import React from "react";
-import { Formik, Form } from "formik";
+import React, { useEffect, useState } from "react";
+import { Formik, Form, Field } from "formik";
 import { Box, Divider, Flex, Text } from "@chakra-ui/layout";
 import FormField from "../../common/FormField";
 import FormTextarea from "../../common/FormTextarea";
 import { Button } from "@chakra-ui/button";
-import { createCommunity } from "../../../api";
+import { createCommunity, getCategories } from "../../../api";
 import { useToast } from "@chakra-ui/toast";
+//@ts-ignore
+import { CategoryType } from "@types/";
+import SelectField from "@components/common/SelectField";
 
 export default function NewCommunityForm() {
+  let { categories: data } = getCategories();
+  const [categories, setCategories] = useState<CategoryType[]>([]);
   const toast = useToast();
+
+  useEffect(() => {
+    const newCategories: { label: string; value: string }[] = [];
+    if (data) {
+      data.forEach(({ name, value }: { name: string; value: string }) => {
+        newCategories.push({ label: name, value: value });
+      });
+    }
+    setCategories(newCategories);
+  }, [data]);
 
   return (
     <>
@@ -17,7 +32,7 @@ export default function NewCommunityForm() {
       <Divider />
       <br />
       <Formik
-        initialValues={{ name: "", description: "" }}
+        initialValues={{ name: "", description: "", category: "" }}
         onSubmit={async (data) => {
           const response = await createCommunity(data);
           if (response.statusText === "OK") {
@@ -38,7 +53,7 @@ export default function NewCommunityForm() {
           }
         }}
       >
-        {({ errors }) => (
+        {({ errors, touched }) => (
           <Form>
             <Flex direction="column" gridGap={2}>
               <FormField
@@ -56,7 +71,15 @@ export default function NewCommunityForm() {
                 description="This is how new members come to understand your community."
                 required
               />
-              <Box mb="50px"></Box>
+              <Field
+                label="Categories"
+                name="category"
+                options={categories}
+                error={errors.category}
+                touched={touched.category}
+                component={SelectField}
+              />
+              <Box mb="80px"></Box>
               <Flex
                 position="absolute"
                 w="100%"
@@ -77,6 +100,7 @@ export default function NewCommunityForm() {
                     _hover={{}}
                     _active={{}}
                     borderRadius={3}
+                    fontSize={14}
                   >
                     CANCEL
                   </Button>
@@ -88,6 +112,7 @@ export default function NewCommunityForm() {
                     _hover={{}}
                     _active={{}}
                     borderRadius={3}
+                    fontSize={14}
                   >
                     CREATE COMMUNITY
                   </Button>
