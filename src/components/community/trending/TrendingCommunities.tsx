87180db0a651287cb96cb3dcc304bcc6b6e8b8d0
Zura <zura.kurta@gmail.com>
2021-04-09 21:44:44 +0400

    :bug: #51 - Minor changes.

--- src/components/community/trending/TrendingCommunities.tsx
+++ src/components/community/trending/TrendingCommunities.tsx
@@ -1,38 +1,25 @@
-import { Box, Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 import { TrendingCommunity } from "../trending/TrendingCommunity";
 import { CommunityType } from "../../../types/index";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { getTrendingCommunities } from "../../../api";
 import StyledBox from "../../common/StyledBox";
 
 export default function TrendingCommunities() {
-  const [communities, setCommunities] = useState<undefined | CommunityType[]>();
+  const { communities } = getTrendingCommunities(4);
 
-  useEffect(() => {
-    fetchCommunities();
-  }, []);
-
-  const fetchCommunities = async () => {
-    const response = await getTrendingCommunities();
-    if (response && response.statusText === "OK") {
-      setCommunities(response.data);
-    }
-  };
-
-  return (
+  return communities && communities.length >= 1 ? (
     <StyledBox>
-      {communities && (
-        <>
-          <Text fontWeight="bold" fontSize={14}>
-            Trending Communities
-          </Text>
-          <Flex mt="15px" direction="column" gridGap={5}>
-            {communities.map((community) => (
-              <TrendingCommunity key={community._id} community={community} />
-            ))}
-          </Flex>
-        </>
-      )}
+      <Text fontWeight="bold" fontSize={14}>
+        Trending Communities
+      </Text>
+      <Flex mt="15px" direction="column" gridGap={5}>
+        {communities.map((community: CommunityType) => (
+          <TrendingCommunity key={community._id} community={community} />
+        ))}
+      </Flex>
     </StyledBox>
+  ) : (
+    <></>
   );
 }
