a7efe199c2e37eff6ba12c8f65b904402c264da8
Zura <zura.kurta@gmail.com>
2021-04-12 17:55:34 +0400

    :sparkles: Make StyledBox more reusable.

--- src/components/common/StyledBox.tsx
+++ src/components/common/StyledBox.tsx
@@ -3,12 +3,41 @@ import React from "react";
 
 export default function StyledBox({
   children,
+  title,
+  titleBackground,
   ...otherProps
 }: {
   children: any;
+  title?: string;
+  titleBackground?: string;
   [x: string]: any;
 }) {
-  return (
+  return title && titleBackground ? (
+    <Box
+      bg="white"
+      overflow="hidden"
+      borderWidth="1px"
+      borderColor="gray.300"
+      borderRadius="4px"
+      position="relative"
+      {...otherProps}
+    >
+      <Box
+        w="100%"
+        color="white"
+        fontWeight="bold"
+        fontFamily="mono"
+        fontSize={14}
+        bg={titleBackground}
+        h="max-content"
+        px="15px"
+        py="10px"
+      >
+        {title}
+      </Box>
+      <Box p="15px">{children}</Box>
+    </Box>
+  ) : (
     <Box
       bg="white"
       overflow="hidden"
