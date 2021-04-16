0ccdba3db84f151337c0acf6383bd94945d54dcf
Zura <zura.kurta@gmail.com>
2021-04-16 23:51:46 +0400

    :sparkles: #81 - Add upload button to Community Picture.

--- src/components/community/common/CommunityPicture.tsx
+++ src/components/community/common/CommunityPicture.tsx
@@ -1,8 +1,12 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import Profile from "../../common/Profile";
+import SVG from "react-inlinesvg";
 
 import { Link } from "react-router-dom";
+//@ts-ignore
+import { getRoleInCommunity } from "@api/";
+import UploadImage from "@assets/upload.svg";
 const randomColor = require("randomcolor");
 
 export default function CommunityPicture({
@@ -10,15 +14,18 @@ export default function CommunityPicture({
   communityUsername,
   width = "80px",
   withLink = false,
+  uploadButton = false,
   ...otherProps
 }: {
   imageSrc: string;
   communityUsername: string;
   width?: string;
   withLink?: boolean;
+  uploadButton?: boolean;
   [x: string]: any;
 }) {
   const numWidth = parseInt(width.slice(0, width.length - 2));
+  const { role } = getRoleInCommunity(communityUsername);
 
   return (
     <Box {...otherProps}>
@@ -30,6 +37,39 @@ export default function CommunityPicture({
         ) : (
           <Profile imageSrc={imageSrc} width={width} isZoomable={false} />
         )
+      ) : uploadButton && role === "admin" ? (
+        <Flex
+          direction="column"
+          position="relative"
+          justifyContent="center"
+          gridGap={2}
+          m={1}
+        >
+          <Box
+            bg="white"
+            borderRadius={50}
+            p="12px"
+            border={`4px solid #0079D3`}
+          >
+            <SVG src={UploadImage} width="40px" height="40px" fill="#0079D3" />
+          </Box>
+          <Button
+            bg="none"
+            p={0}
+            _hover={{}}
+            _active={{}}
+            _focus={{}}
+            fontSize="11.5px"
+            color="#0079D3"
+            h="max-content"
+            position="absolute"
+            left="50%"
+            transform="translateX(-50%)"
+            bottom="-17px"
+          >
+            Add icon
+          </Button>
+        </Flex>
       ) : (
         <Box
           w={width}
