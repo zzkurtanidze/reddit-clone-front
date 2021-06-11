5e6db2e18be59d47836453e088f5f62ef1964589
Zura Kurtanidze <zura.kurta@gmail.com>
2021-06-11 19:56:56 +0400

    :boom: #108 - Hide community if it's private and user is not member.

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -1,5 +1,5 @@
 //@ts-nocheck
-import { Box, Flex, Grid, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, Image, Text } from "@chakra-ui/react";
 import React, { useContext, useEffect, useState } from "react";
 import { getCommunity } from "@api";
 import Container from "@components/common/Container";
@@ -12,7 +12,7 @@ import CommunityInfo from "@components/community/common/CommunityInfo";
 import { UserContext } from "@context/UserContext";
 import Loading from "@components/common/Loading";
 import FixedElement from "@components/common/FixedElement";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import ErrorPage from "@pages/error";
 import Moderators from "@components/community/common/Moderators";
 import { getRoleInCommunity } from "@api/";
@@ -20,6 +20,12 @@ import GrowCommunity from "@components/community/common/GrowCommunity";
 import StyleCommunity from "@components/community/common/StyleCommunity";
 import CommunityFlairs from "@components/community/common/CommunityFlairs";
 import queryString from "query-string";
+import PrimaryButton from "@components/common/PrimaryButton";
+import SecondaryButton from "@components/common/SecondaryButton";
+import { responseOnJoinRequest } from "@api/";
+import { joinCommunity } from "@api/";
+import { sendNotification } from "@api/";
+import { UserType } from "@types/";
 
 export default function CommunityPage({ match }: { match: any }) {
   const [joined, setJoined] = useState<boolean>(false);
@@ -28,17 +34,33 @@ export default function CommunityPage({ match }: { match: any }) {
   const { community, isLoading, error } = getCommunity(name);
   const { role } = getRoleInCommunity(name);
   const [posts, setPosts] = useState<[]>([]);
+  const [privacy, setPrivacy] = useState("public");
+  const [requested, setRequested] = useState(false);
 
+  const history = useHistory();
   const params = queryString.parse(window.location.search);
 
   useEffect(() => {
     if (community) document.title = `r/${community?.name}`;
-    if (user?.joined && community) {
-      user.joined.forEach((joinedCommunity) => {
-        if (joinedCommunity._id === community._id) {
-          setJoined(true);
-        }
-      });
+
+    let joined = false;
+
+    if (community && user) {
+      if (user?.joined) {
+        user.joined.forEach((joinedCommunity) => {
+          if (joinedCommunity._id === community._id) {
+            joined = true;
+            setJoined(joined);
+          }
+        });
+      }
+      if (
+        user.pendingCommunities &&
+        user.pendingCommunities.indexOf(community._id) >= 0
+      ) {
+        setRequested(true);
+      }
+      setPrivacy("private");
     }
   }, [user, community]);
 
@@ -56,8 +78,101 @@ export default function CommunityPage({ match }: { match: any }) {
 
   if (error) return <ErrorPage />;
   if (isLoading) return <Loading />;
+  if (privacy === "private")
+    return (
+      <Flex
+        w="100%"
+        h="100vh"
+        bg="#dae0e6"
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        gridGap={7}
+      >
+        <Image
+          src="http://localhost:4000/assets/key.svg"
+          w="100px"
+          h="100px"
+          borderRadius={50}
+        />
+        <Text fontFamily="mono" fontWeight="bold">
+          r/{community.username} is a private community
+        </Text>
+        <Box
+          w="500px"
+          h="max-content"
+          px={5}
+          py={2}
+          bg="white"
+          borderRadius={5}
+        >
+          <Text
+            fontSize={18}
+            fontFamily="mono"
+            textAlign="center"
+            fontWeight="bold"
+          >
+            r/{community.username}
+          </Text>
+          <br />
+          <Text fontSize={14} fontFamily="mono" fontWeight="medium">
+            {community.description}
+          </Text>
+        </Box>
+        <Text
+          fontFamily="mono"
+          fontSize={16}
+          fontWeight="semibold"
+          w="525px"
+          textAlign="center"
+        >
+          The moderators of r/rameprosta have set this community as private.
+          Only approved members can view and take part in its discussions.
+        </Text>
+        <Flex gridGap={5}>
+          <SecondaryButton
+            label={requested ? "Pending for approval" : "Request to join"}
+            onClick={async () => {
+              if (user) {
+                await joinCommunity(community._id);
+                community.moderators.forEach(async (moderator: UserType) => {
+                  await sendNotification(moderator.username, {
+                    title: `u/${user.username} requested to join r/${community.username} community`,
+                    description: "",
+                    type: "request",
+                    more: {
+                      community: community._id,
+                      url: `/r/${community.username}/about/pending`,
+                    },
+                  });
+                });
+                setRequested(true);
+              }
+            }}
+            bg="none"
+            disabled={requested}
+          />
+          <PrimaryButton
+            label="Browse reddit"
+            onClick={() => history.push("/")}
+          />
+        </Flex>
+        <Text
+          w="500px"
+          color="gray.500"
+          fontWeight="medium"
+          fontSize={12}
+          fontFamily="mono"
+          textAlign="center"
+        >
+          Use of this site constitutes acceptance of our User Agreement and
+          Privacy Policy . ©2021 reddit inc. All rights reserved. REDDIT and the
+          ALIEN Logo are registered trademarks of reddit inc.
+        </Text>
+      </Flex>
+    );
   return (
-    <>
+    <Box>
       ,
       {community && (
         <Box>
@@ -162,6 +277,6 @@ export default function CommunityPage({ match }: { match: any }) {
           </Container>
         </Box>
       )}
-    </>
+    </Box>
   );
 }
